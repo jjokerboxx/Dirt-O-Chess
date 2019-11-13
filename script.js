f10e184a053706f868f58e40c53606eb8f90f790
Episode <dlrlgns58@gmail.com>
2019-11-13 21:15:48 +0900

    챔피언 합성 기능 추가 및 일부 코드 수정

--- script.js
+++ script.js
@@ -1,29 +1,29 @@
-var cham_list = ['아리', '카사딘', '바루스', '가렌', '워윅', '다리우스', '니달리', '피오라', '렉사이', '베인', '이블린', '카직스'];
+// 롤토체스 바닐라 자바스크립트 제작기
+// 우정잉님의 클립에 영감을 얻기 토이프로젝트오 시작
+// 자바스크립트에 대한 공부와 꾸준한 개발을 위해 프로젝트에 임함
+// 꼼꼼하게 설계하지 않아 추후에 대규모의 리펙토링이 필요
+// 
+// 11. 14. 2019 - 리기훈
 
-var class_list = ['기사', '메이지', '싸움꾼', '검사', '정찰대']
 
-var species_list = ['야생', '공허', '악마', '귀족']
-
-
-bench_list = []
-
+// 골드와 마나
 var gold = 100;
 const mana = 100;
 
+
+// 챔피언 객체 생성 클래스
+// template :  { _name : '', _class : '', _species : '', ...}
 class Champion
 {
-	champion_constructor(name, price, rank, hp, atk, atk_speed)
+	constructor(name, class_, species, price, rank, hp, atk, atk_speed)
 	{
 		this.name = name;
 		this.price = price;
 		this.rank = rank;
-	
-		this.hp = hp;
-		
+		this.class_ = class_;
+		this.hp = hp;		
 		// this.mana-generation = m_g;
-	
-		// this.species = species;
-		
+		this.species = species;		
 		this.atk = atk;
 		this.atk_speed = atk_speed; 
 	}
@@ -46,22 +46,43 @@ class Champion
 	}
 }
 
-var Garen = new Champion();
-Garen.champion_constructor('가렌', 1, 'common', 1000, 5, 1);
+
+// 각 챔피언 객체 생성 
+var Garen = new Champion('가렌', '기사', '귀족', 1, 'common', 1000, 5, 1);
+var Ahri = new Champion('아리', '메이지', '야생', 1, 'common', 700, 7, 1);
+var Kassadin = new Champion('카사딘', '메이지', '공허', 1, 'common', 1000, 5, 1);
+var Varus = new Champion('바루스', '정찰대','악마', 1, 'common', 1000, 5, 1);
+var Warwick = new Champion('워윅', '싸움꾼', '야생', 1, 'common', 1000, 5, 1);
+var Darius = new Champion('다리우스', '기사', '제국', 'common', 1000, 5, 1);
+var Nidalee = new Champion('니달리', '싸움꾼', '야생', 1, 'common', 1000, 5, 1);
+var Fiora = new Champion('피오라', '기사', '귀족', 1, 'common', 1000, 5, 1);
+var RekSai = new Champion('렉사이', '싸움꾼', '공허', 1, 'common', 1000, 5, 1);
+var Vayne = new Champion('베인', '정찰대', '귀족', 1, 'common', 1000, 5, 1);
+var Evelynn = new Champion('이블린', '암살자', '악마', 1, 'common', 1000, 5, 1);
+var KhaZix = new Champion('카직스', '암살자', '공허', 1, 'common', 1000, 5, 1);
+
+arr_cham = [Garen, Ahri, Kassadin, Varus, Warwick, Darius, Nidalee, Fiora, RekSai, Vayne, Evelynn, KhaZix]
 
 
 //리롤 버튼
 function reroll()
-{
-	
+{	
 	if (gold >= 2)
 	{
 		gold -= 2;
-		var doc0 = document.getElementsByClassName('buy_cham')[0].innerText = randomItem(cham_list);
-		var doc1 = document.getElementsByClassName('buy_cham')[1].innerText = randomItem(cham_list);
-		var doc2 = document.getElementsByClassName('buy_cham')[2].innerText = randomItem(cham_list);
-		var doc3 = document.getElementsByClassName('buy_cham')[3].innerText = randomItem(cham_list);
-		var doc4 = document.getElementsByClassName('buy_cham')[4].innerText = randomItem(cham_list);
+
+		var a = randomItem(arr_cham);
+		var b = randomItem(arr_cham);
+		var c = randomItem(arr_cham);
+		var d = randomItem(arr_cham);
+		var e = randomItem(arr_cham);		
+
+		// 아래 코드 반복문으로 새로 코딩 하기! for(var i in )
+		var doc0 = document.getElementsByClassName('buy_cham')[0].innerText = a.name + "\n" + a.class_ + "\n" + a.species;
+		var doc1 = document.getElementsByClassName('buy_cham')[1].innerText = b.name + "\n" + b.class_ + "\n" + b.species;
+		var doc2 = document.getElementsByClassName('buy_cham')[2].innerText = c.name + "\n" + c.class_ + "\n" + c.species;
+		var doc3 = document.getElementsByClassName('buy_cham')[3].innerText = d.name + "\n" + d.class_ + "\n" + d.species;
+		var doc4 = document.getElementsByClassName('buy_cham')[4].innerText = e.name + "\n" + e.class_ + "\n" + e.species;
 		// 여기에 나중에 각 챔피언의 속성까지 표기
 		//   + ' ' + randomItem(cham_list) + ' ' + randomItem(cham_list) + " " + randomItem(cham_list) + ' ' + randomItem(cham_list);
 
@@ -71,29 +92,12 @@ function reroll()
 		alert("not enough gold!");
 	}
 	show_gold();
-  // maybe i should use Jquery to run this code in certain block of html
-  
-  // doc0.innerText = document.write(randomItem(cham_list), ' ');
-  // doc1.innerText = document.write(randomItem(cham_list), ' ');
-  // document.write(randomItem(cham_list), '&nbsp;');
-//   document.write(randomItem(cham_list), '&nbsp;');
-//   document.write(randomItem(cham_list), '&nbsp;');
-
 }
 
-// 가렌 연습
-function show_cham()
-{
-		
-	var asdasd = document.getElementById('asdasd');
-  	asdasd.innerText = String(Garen.hp);
-	
-}
 
 //경험치 구매
 function Exp_Up()
-{
-	
+{	
 	if (gold >= 4)
 	{
 		gold -= 4;
@@ -107,6 +111,7 @@ function Exp_Up()
 	{
 		alert("not enough gold!");
 	}
+
 	show_gold();
 }
 
@@ -119,36 +124,137 @@ function show_gold()
 }
 
 //랜덤 챔피언 불러오기
-function randomItem (a)
+function randomItem(a)
 {
 	return a[Math.floor(Math.random() * a.length)];
 }
 
-// 챔피언 구매시 버튼 삭제 액션
+// 챔피언 구매 및 버튼 삭제 액션
 function button_pressed(event)
 {
-	gold -= 4;  //여기에 각 챔피언의 가격
+	//add_bench(event);	
 	var btp = event.currentTarget;
+	gold -= 4;  //여기에 각 챔피언의 가격
+	
+	var bench_chair = document.getElementsByClassName("bench_list");
+	
+	if(bench_chair[9].innerText != "")
+	{
+		alert("not enough bench space!");
+		return false;
+	}
+	for(var i = 0; i < bench_chair.length; i++)
+	{		
+		if(bench_chair[i].innerText == "")
+		{
+			bench_chair[i].innerText = btp.innerText;
+			
+			break;
+			console.log("asd");
+		}		
+	}
+			
 	btp.innerText = " ";
+	
 	show_gold();
+	Upgrade();
+	// var bench[] = document.getElementsByClassName("bench_list");  여기에 벤치리스트 생성 큐 생성
+}
+
+// 구매할 때 동시에 발생하기에 구매 함수에 넣어버림
+// 챔피언 3개 모일 시 업그레이드 액션
+function Upgrade(){
+	var counts = {};
+	var TextArray = [];
+	var bench_chair = document.getElementsByClassName("bench_list");	
+
+	for (var i = 0; i < bench_chair.length; i++)
+	{
+		TextArray[i] = bench_chair[i].innerText;		
+	}
+
+	// count 구문
+	TextArray.forEach(function(x){ counts[x] = (counts[x] || 0) + 1; });
+	console.log(counts);
+	var asd = Object.keys(counts);
+	var isthree = function (value)
+	{
+		if(value == "")
+		{
+			return false;
+		}
+		return counts[value] == 3;
+	}
+
+	// 합성 부분
+	if (asd.some(isthree))
+	{
+		var ddd = [];
+		asd.forEach(function(x){ if(counts[x] == 3){ ddd.push(x); } });
+		console.log(ddd[0]);
+		
+		for(var o = 0; bench_chair.length; o++)
+		{
+			if( bench_chair[o].innerText == ddd[0] )
+			{
+				bench_chair[o].innerText = ddd[0] + "\n" +"Upgraded";
+				break;
+			}
+		}
+		setTimeout( function(){
+			for(var x = 0; bench_chair.length; x++)
+			{
+				if (bench_chair[x].innerText == ddd[0])
+				{
+					// 지금 크래시가 발생함 어떤 부분을 수정해야하는가 고민해보자
+					bench_chair[x].innerText = "";
+					
+				}
+			}
+		}			
+		, 500);
+		
+		console.log("oh yeag!");
+	}
+}
+
+// 전장으로 내보내는 함수
+function call_of_the_duty(event){
+
+	// 해당 이벤트는 드래그 앤 드롭으로도 구현이 가능해야함
+	var asdasd = event.currentTarget;
+	
 }
 
 // 시작시 초기화할 것들
 function when_load()
 {
+	var user = prompt("please tell your name here");
+	document.getElementById("welcome").innerText = user;
 	show_gold();
-	var doc0 = document.getElementsByClassName('buy_cham')[0].innerText = randomItem(cham_list);
-	var doc1 = document.getElementsByClassName('buy_cham')[1].innerText = randomItem(cham_list);
-	var doc2 = document.getElementsByClassName('buy_cham')[2].innerText = randomItem(cham_list);
-	var doc3 = document.getElementsByClassName('buy_cham')[3].innerText = randomItem(cham_list);
-	var doc4 = document.getElementsByClassName('buy_cham')[4].innerText = randomItem(cham_list);
+	reroll();
 
-	
+	draw();
+	// show_Garen();
 }
-// function Upgrade(cham)
+
+// 구매시 벤치에 해당 챔피언 정보 추가
+//function add_bench(event)
 // {
-// 	cham.hp, atk, atk_speed *3;
+// 	var bnh = event.currentTarget;
+	
+// 	//var benchList = document.getElementsByClassName("buy_cham");
+// 	var bench_chair = document.getElementsByClassName("bench_list");
+	
+// 	for(var i in bench_chair)
+// 	{
+// 		if(i.innerText = "")
+// 		i.innerText = bnh.innerText;
+// 		break;
+// 	}
+// 	// 이건 포문으로 하면 안됨, 큐를 생각해보자.
 // }
+
 
 // function Purchase(cham)
 // {
@@ -166,3 +272,16 @@ function when_load()
 // 	gold += cham.price;
 		
 // }
+
+
+// 화면에 그림 생성
+var cvs = document.querySelector('canvas');
+// var canvas = document.getElementById("field");
+function draw()
+{
+	var ctx = cvs.getContext("2d");
+	var fire = document.getElementById("asdasdd");
+	ctx.drawImage(fire, 10, 10);
+	// ctx.fillRect(100, 100, 100, 100);
+}
+
